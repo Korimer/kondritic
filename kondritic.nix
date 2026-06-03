9057b0584827a21efd6b88c18a611e9caad5d89c
= <=>
2026-06-03 16:22:06 -0600

    shifting to mapattrs'

--- kondritic.nix
+++ kondritic.nix
@@ -1,4 +1,27 @@
-args: module:
-  args.nixpkgs.lib.mapAttrs
-  (key: val: val)
-  module
+let
+  options = {
+    #nixos = (builtins.getFlake (toString ./.)).nixosConfigurations.<hostname>.options;
+    #home = (builtins.getFlake (builtins.toString ./.)).nixosConfigurations.<name>.options.home-manager.users.type.getSubOptions []    };
+  };
+
+  nixosfinal = { inputs, ... }: inputs.nixpkgs.lib.nixosSystem options.nixos;
+
+  homefinal = {};
+  optionsfinal = {
+    den._internal.options = {
+      nixos = nixosfinal.options;
+      home = nixosfinal.options.home-manager.users.type.getSubOptions [];
+    };
+  };
+
+  kon-out = 
+    nixosfinal
+    // homefinal
+    // optionsfinal
+  ;
+in
+
+args: module: config:
+  args.nixpkgs.lib.mapAttrs'
+    (key: val: {name = key; value = val;})
+    (module {})
