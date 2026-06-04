9581054fe18a9b5e133fa94665b980fa9209200a
= <=>
2026-06-04 16:49:04 -0600

    working path to attrs

--- flake.nix
+++ flake.nix
@@ -5,7 +5,5 @@
     home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = inputs@{ ... }: {
-    with-kontext = (import ./kondritic.nix);
-  };
+  outputs = inputs@{ ... }: (import ./kondritic.nix { inherit inputs; });
 }
--- kondritic.nix
+++ kondritic.nix
@@ -1,20 +1,4 @@
-let 
-  # directory reading
-  importDir = lib: directory: lib.flatten (
-    lib.pipe directory [
-      builtins.readDir
-      (lib.filterAttrs (name: type: type == "directory" || lib.hasSuffix ".nix" name))
-      (lib.filterAttrs (name: _: !(lib.hasPrefix "_" name)))
-      (lib.mapAttrsToList (
-        name: type: if type == "directory"
-          then importDir (directory + ("/" + name))
-          else directory + ("/" + name)
-      ))
-    ]
-  );
-in
-
-inputs: module: config:
+{ inputs }:
 let
   konlib = {
     mkHost = options:
@@ -29,14 +13,14 @@ let
 
     _pathToAttrs = escapedPath:
       let
-        match = builtins.match "([^/]+)/(.*)";
+        match = builtins.match "([^/]+)/(.*)" escapedPath;
       in
         if match == null then
-          {}
+          { ${escapedPath} = {}; }
         else
           {
-            ${builtins.head match} =
-              konlib._pathToAttrs (builtins.tail match);
+            ${builtins.elemAt match 0} =
+              konlib._pathToAttrs (builtins.elemAt match 1);
           }
       ;
 
@@ -49,40 +33,46 @@ let
       
   };
 
-  hosts = config.hosts;
-  users = config.users;
+  with-kontext = { config, module }:
+  let
 
-  options = {
-    #nixos = (builtins.getFlake (toString ./.)).nixosConfigurations.<hostname>.options;
-    #home = (builtins.getFlake (builtins.toString ./.)).nixosConfigurations.<name>.options.home-manager.users.type.getSubOptions []    };
+    hosts = config.hosts;
+    users = config.users;
+
+    options = {
+      #nixos = (builtins.getFlake (toString ./.)).nixosConfigurations.<hostname>.options;
+      #home = (builtins.getFlake (builtins.toString ./.)).nixosConfigurations.<name>.options.home-manager.users.type.getSubOptions []    };
+      };
+
+      lol = inputs.nixpkgs.lib.mapAttrs'
+        (key: val: {name = key; value = val;})
+        (module {})
+      ;
+
+      nixConfigArr = map
+        (hostname:
+          {name = hostname; value = konlib.mkHost inputs.nixpkgs [lol];}
+        )
+        hosts
+      ;
+
+      allNixConfigs = inputs.nixpkgs.lib.listToAttrs nixConfigArr;
+      
+      allNixOptions = inputs.nixpkgs.lib.mapAttrs
+        (key: val: val.options )
+        allNixConfigs
+      ;
+
+      configsFinal = { nixosConfigurations = allNixConfigs; };
+
+  in configsFinal;
+
+  kon-out = {
+    kon = {
+      #options.nixos = with-kontext.allNixOptions;
+      lib = konlib;
     };
+    with-kontext = with-kontext;
+  };
 
-    lol = inputs.nixpkgs.lib.mapAttrs'
-      (key: val: {name = key; value = val;})
-      (module {})
-    ;
-
-    nixConfigArr = map
-      (hostname:
-        {name = hostname; value = konlib.mkHost inputs.nixpkgs [lol];}
-      )
-      hosts
-    ;
-
-    allNixConfigs = inputs.nixpkgs.lib.listToAttrs nixConfigArr;
-    
-    allNixOptions = inputs.nixpkgs.lib.mapAttrs
-      (key: val: val.options )
-      allNixConfigs
-    ;
-
-    configsFinal = { nixosConfigurations = allNixConfigs; };
-
-    kon-out = 
-      configsFinal
-      // { kon.options.nixos = allNixOptions; }
-      // { kon.lib = konlib; }
-    ;
-
-  in kon-out
-
+in kon-out
